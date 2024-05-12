6cf3fdd3e021ae1ee6026d91fe39fbdfbabef430
Samuel Oates <samuelroates@gmail.com>
2024-05-12 17:57:05 -0400

    new stuff

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const path = require("path");
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 80;
 const bodyParser = require("body-parser");
 require("dotenv").config();
 
